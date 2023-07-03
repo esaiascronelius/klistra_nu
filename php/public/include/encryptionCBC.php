ca10eb16215e18c0f061801d58071a4a5852a195
Esaias Cronelius <esaias@cronelius.se>
2023-07-03 23:04:23 +0200

    Added GCM encryption

--- php/public/include/encryptionCBC.php
+++ php/public/include/encryptionCBC.php
@@ -1,42 +1,39 @@
 <?php
 class Encryption
 {
-    private $cipher = "AES-256-GCM";
+    private $cipher = "AES-256-CBC";
     private $key;
     private $iv;
 
     public function __construct($key)
     {
         $this->key = hash("sha256", $key, true);
+        $this->iv = random_bytes(16);
     }
 
     public function encrypt($data)
     {
-        $this->iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($this->cipher));
-        $tag = "";
         $encrypted = openssl_encrypt(
             $data,
             $this->cipher,
             $this->key,
-            OPENSSL_RAW_DATA,
-            $this->iv,
-            $tag
+            0,
+            $this->iv
         );
-        $encoded = base64_encode($encrypted . "::" . $this->iv . "::" . $tag);
+        $encoded = base64_encode($encrypted . "::" . $this->iv);
         return $encoded;
     }
 
     public function decrypt($data)
     {
         $decoded = base64_decode($data);
-        list($encrypted, $iv, $tag) = explode("::", $decoded, 3);
+        list($encrypted, $iv) = explode("::", $decoded, 2);
         $decrypted = openssl_decrypt(
             $encrypted,
             $this->cipher,
             $this->key,
-            OPENSSL_RAW_DATA,
-            $iv,
-            $tag
+            0,
+            $iv
         );
         return $decrypted;
     }
